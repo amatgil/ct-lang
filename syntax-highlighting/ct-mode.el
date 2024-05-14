31261eb442ea3f34e959d5e321c2fbfcd5bb05e7
amatgil <amatgilvinyes@gmail.com>
2024-05-14 17:00:47 +0200

    Progress on syntax highlighting

--- syntax-highlighting/ct-mode.el
+++ syntax-highlighting/ct-mode.el
@@ -1,32 +1,35 @@
 ;;; ct-mode.el --- major mode for ç-lang
 
-;(define-generic-mode ct-mode
-;   (list "<--" "--")                                           ; Comments
-;   (list "deffun" "deftype" "deftypeclass" "loop" "match")     ; Keyword
-;  '(("true" . 'font-lock-constant-face)                        ; font lock
-;    ("false" . 'font-lock-constant-face))
-;   (list "*.ct")                                               ; automode lock
-;   nil
-;   "Major mode for editing ç-lang files")
+(setq debug-on-error t)
 
 (defconst ct-mode-syntax-table
   (let ((table (make-syntax-table)))
-    (modify-syntax-entry ?' "\"" table)  ; Single quote -> string
     (modify-syntax-entry ?\" "\"" table) ; Double quote -> string
     (modify-syntax-entry ?\n ">" table)  ; Newline finishes comment
+    table
     ))
 
 (define-derived-mode ct-mode
-  prog-mode "Ç-lang"
-  "Major mode for Ç-lang"
-  :syntax-table ct-mode-syntax-table
-  (font-lock-fontify-buffer))
+   prog-mode "Ç-lang"
+   "Major mode for Ç-lang"
+   :syntax-table ct-mode-syntax-table
+   (set (make-local-variable 'font-lock-defaults) '(ct-font-lock t nil nil)))
 
+(defconst ct-keywords (concat "\\<" (regexp-opt '("deffun" "deftype" "deftypeclass" "if" "match" "where" "impls" "let")) "\\>"))
+
+(defconst ct-font-lock
+  `(
+    ("<--.*" . font-lock-comment-face)
+    ("{-.*-}" . font-lock-comment-face)
+    ("\".*\"" . font-lock-string-face)
+    ("[?[A-Z][a-zA-Z0-9_']*" . font-lock-type-face)
+    (,ct-keywords . font-lock-keyword-face) 
+    ))
 
 ; After running eval-buffer and ct-mode, you should be able to see the changes here (because a semicolon isn't a comment in it!)
-;(deftype (List a)       <-- List with typed admitted by the type constructor
-;  '(Empty               <-- possible variants of sum type
-;   (Cons a (List a))))
+;(deftype (List a)         <-- List with typed admitted by the type constructor
+;  '((Empty)               <-- possible variants of sum type
+;    (Cons a (List a))))
 ;
 ;(deffun foldl
 ;  (Fn[a a a] a (List a))
@@ -36,18 +39,17 @@
 ;    (foldl 'f (f acc (head xs)) (tail xs))))
 ;
 ;(deffun empty?
-;  ((List a) bool)
+;  ((List a) Bool)
 ;  (xs)
 ;  (match xs 
 ;    ((Empty) true)
 ;    ((Cons x t) false)))
 ;
 ;(deffun append
-;  ((List a))
-;  (xss)
-;  (match xss
-;   ((Empty) (Empty)
-;    (Cons xs rest) (append xs (flatten xss)))))
+;  (a (List a) (List a))
+;  (x xs)
+;  (Cons x xs))
+;   
 ;
 ;(deffun flatten
 ;  ((List (List a)) (List a))
@@ -55,3 +57,8 @@
 ;  (match xss
 ;   ((Empty) (Empty)
 ;    (Cons xs rest) (append xs (flatten xss)))))
+;
+;(deffun stringtest
+;  (Bool)
+;  (b)
+;  (if b "It's true :D" "It's false D:"))
