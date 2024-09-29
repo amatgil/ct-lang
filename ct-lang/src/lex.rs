e85144939e7a20d7f4bed82b8172dd8f2e80d463
amatgil <amatgilvinyes@gmail.com>
2024-09-29 18:28:28 +0200

    Thinking about what a Token is

--- ct-lang/src/lex.rs
+++ ct-lang/src/lex.rs
@@ -5,12 +5,23 @@ Good example: https://github.com/kaikalii/cube/blob/master/src/lex.rs
 */
 
 // TODO
-enum Token {
+enum Token<'a> {
+    ParenOpen,
+    ParenClose,
+    CommentStart,
+    DoccommentStart,
+    Identifier(&'a str)
 }
 
 enum LexError {
 }
 
-fn lex() -> Result<Token, LexError> {
+type TokensRes<'a> = Result<Vec<Token<'a>>, LexError>;
+
+fn lex(input: &str) -> TokensRes<'_> {
+    todo!()
+}
+
+fn lex_deffun(input: &str) -> TokensRes<'_> {
     todo!()
 }
